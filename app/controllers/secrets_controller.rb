3c7e809a30f4b2be5005806d0d7759833719b829
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-12 16:31:43 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,5 +1,12 @@
 class SecretsController < ApplicationController
+  before_action :show, :check_login
+  
   def show
-    redirect_to sessions_new_path if !session[:name]
+  end
+
+  private
+
+  def check_login
+    redirect_to sessions_new_path if !current_user
   end
 end
